4ea567c7660cc75f4ff5c7ada386d723328295f1
EC2 Default User <ec2-user@ip-172-31-12-31.ap-northeast-1.compute.internal>
2023-08-06 10:31:58 +0000

    [Add]view{users(index/show)books(index/show)}/controller{forwardviews and bookscreate}

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,29 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book2 = Book.new
+    @user = @book.user
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
+    
   end
   
   def update
@@ -15,4 +33,11 @@ class BooksController < ApplicationController
   def destroy
     
   end 
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @book = Book.new
   end
 
   def show
+    user = User.find(params[:id])
+    @book = Book.new
+    @books = user.books.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
